49324701d853ed7435c02cf435d480834a1c4272
Diego Vásquez <101302838+DiegVas@users.noreply.github.com>
2024-09-18 23:13:04 -0600

    Eliminacion de javascript en el codigo

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route("/file")
+def file():
+    return render_template("/layouts/File.html")
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("/layouts/Process.html")
+
+
+@app.route("/help")
+def help():
+    return render_template("/layouts/Report.html")
 
 
 if __name__ == "__main__":
